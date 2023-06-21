0a80326d22fb8f6db14bc68dd7ab242018bb2fb5
stephenbeese <stephenbeese1@live.co.uk>
2023-06-21 12:24:19 +0000

    Add checkout views and templates

--- checkout/signals.py
+++ checkout/signals.py
@@ -3,7 +3,7 @@ from django.dispatch import receiver
 
 from .models import OrderLineItem
 
-@reciever(post_save, sender=OrderLineItem)
+@receiver(post_save, sender=OrderLineItem)
 def update_on_save(sender, instance, created, **kwargs):
     """
     Update order total on lineitem update/create
@@ -11,7 +11,7 @@ def update_on_save(sender, instance, created, **kwargs):
     instance.order.update_total()
 
 
-@reciever(post_delete, sender=OrderLineItem)
+@receiver(post_delete, sender=OrderLineItem)
 def update_on_save(sender, instance, **kwargs):
     """
     Update order total on lineitem delete
