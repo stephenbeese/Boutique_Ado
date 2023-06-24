4aa10b6bf8d0f3fdfb119e809089953d6e6c479d
stephenbeese <stephenbeese1@live.co.uk>
2023-06-24 10:36:19 +0000

    Add checkout success logic

--- checkout/models.py
+++ checkout/models.py
@@ -8,7 +8,6 @@ from products.models import Product
 
 
 class Order(models.Model):
-
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
@@ -35,7 +34,7 @@ class Order(models.Model):
         Update grand total each time a line item is added,
         accounting for delivery costs
         """
-        self.order_total = self.lineitems.aggreage(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
@@ -43,7 +42,6 @@ class Order(models.Model):
         
         self.grand_total = self.order_total + self.delivery_cost
         self.save()
-
 
     def save(self, *args, **kwargs):
         """
@@ -53,7 +51,7 @@ class Order(models.Model):
         if not self.order_number:
             self.order_number = self._generate_order_number()
 
-        super.save(*args, **kwargs)
+        super().save(*args, **kwargs)
     
     def __str__(self):
         return self.order_number
@@ -72,7 +70,7 @@ class OrderLineItem(models.Model):
         and update the order total
         """
         self.lineitem_total = self.product.price * self.quantity
-        super.save(*args, **kwargs)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f'SKU {self.product.sku} on order {self.order.order_number}'
--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,41 +1,34 @@
-/**
- * Core logic/payment flow for this comes from here:
- * https://stripe.com/docs/payments/accept-a-payment?platform=web&ui=elements
- * 
- * CSS from here:
- * https://stripe.com/docs/stripe-js
- */
+/*
+    Core logic/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+
+    CSS from here: 
+    https://stripe.com/docs/stripe-js
+*/
 
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
-
 var style = {
     base: {
-        color: "#000",
+        color: '#000',
         fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-        fontSmoothing: "antialiased",
-        fontSize: "16px",
-        "::placeholder": {
-            color: "#aab7c4"
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
         }
     },
     invalid: {
-        //   fontFamily: 'Arial, sans-serif',
-        color: "#dc35545",
-        iconColor: "#dc3545"
+        color: '#dc3545',
+        iconColor: '#dc3545'
     }
 };
-
-var card = elements.create('card', {
-    style: style
-});
-
+var card = elements.create('card', {style: style});
 card.mount('#card-element');
 
 // Handle realtime validation errors on the card element
-
 card.addEventListener('change', function (event) {
     var errorDiv = document.getElementById('card-errors');
     if (event.error) {
@@ -51,43 +44,32 @@ card.addEventListener('change', function (event) {
     }
 });
 
-// Handle form submit 
-const form = document.getElementById('payment-form');
+// Handle form submit
+var form = document.getElementById('payment-form');
 
-form.addEventListener('submit', async (event) => {
-    event.preventDefault();
-    card.update({
-        'disabled': true
-    });
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true});
     $('#submit-button').attr('disabled', true);
-    const {
-        error
-    } = await stripe.confirmPayment(clientSecret, {
-        //`Elements` instance that was used to create the Payment Element
-        elements,
-        confirmParams: {
-            return_url: 'https://example.com/order/123/complete',
-        },
-    });
-
-    if (error) {
-        var errorDiv = document.getElementById('card-errors');
-        var html = `
-            <span class="icon" role="alert">
-                <i class="fas fa-times"></i>
-            </span>
-            <span>${error.message}</span>
-        `;
-        $(errorDiv).html(html);
-        card.update({'disabled': false});
-        $('#submit-button').attr('disabled', false);
-    } else {
-
-        if (paymentIntent.status === 'succeeded') {
-            form.submit()
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
         }
-        // Your customer will be redirected to your `return_url`. For some payment
-        // methods like iDEAL, your customer will be redirected to an intermediate
-        // site first to authorize the payment, then redirected to the `return_url`.
-    }
+    }).then(function(result) {
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
 });
