23f9a125307c77e159d0e62af85b941a17b111ec
stephenbeese <stephenbeese1@live.co.uk>
2023-07-13 17:19:10 +0000

    Add view, url and template for admin product management

--- products/views.py
+++ products/views.py
@@ -1,73 +1,10 @@
-# from django.shortcuts import render, redirect, reverse, get_object_or_404
-# from django.contrib import messages
-# from django.db.models import Q
-# from .models import Product
-
-
-# # def all_products(request):
-# #     """ A view to show individual product details """
-# #     products = Product.objects.all()
-# #     query = None
-
-# #     if request.GET:
-# #         if 'q' in request.GET:
-# #             query = request.GET['q']
-# #             if not query:
-# #                 messages.error(request, "you didn't enter any search criteria!")
-# #                 return redirect(reverse('products'))
-
-# #             queries = Q(name__icontains=query) | Q(description__icontains=query)
-# #             products = products.filter(queries)
-
-# #     context = {
-# #         'products': products,
-# #         'search_term': query,
-# #     }
-# #     return render(request, 'products/products.html', context)
-
-# def all_products(request):
-#     """ A view to show all products, including sorting and search queries """
-
-#     products = Product.objects.all()
-#     query = None
-
-#     if request.GET:
-#         if 'q' in request.GET:
-#             query = request.GET['q']
-#             if not query:
-#                 messages.error(request, "You didn't enter any search criteria!")
-#                 return redirect(reverse('products'))
-
-#             # queries = Q(name__icontains=query) | Q(description__icontains=query)
-
-#             queries = Q(name__icontains=query) | Q(description__icontains=query)
-#             products = products.filter(queries)
-
-#     context = {
-#         'products': products,
-#         'search_term': query,
-#     }
-
-#     return render(request, 'products/products.html', context)
-
-
-# def product_detail(request, product_id):
-#     """ A view to show all products, including sorting and search queries """
-#     product = get_object_or_404(Product, pk=product_id)
-
-#     context = {
-#         'product': product
-#     }
-#     return render(request, 'products/product_detail.html', context)
-
-
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
-from .models import Product, Category
 
-# Create your views here.
+from .models import Product, Category
+from .forms import ProductForm
 
 
 def all_products(request):
@@ -132,3 +69,15 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
