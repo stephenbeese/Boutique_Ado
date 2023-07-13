8194da664b2e1798236daa82a2511fc873bcdb18
stephenbeese <stephenbeese1@live.co.uk>
2023-07-13 15:25:04 +0000

    Update webhook handler to handle profile

--- checkout/views.py
+++ checkout/views.py
@@ -161,6 +161,7 @@ def checkout_success(request, order_number):
                 'default_street_address2': order.street_address2,
                 'default_county': order.county,
             }
+
             user_profile_form = UserProfileForm(profile_data, instance=profile)
             if user_profile_form.is_valid():
                 user_profile_form.save()
