ab2276c559cd8d23d857edb75932d5ed00ad7f51
stephenbeese <stephenbeese1@live.co.uk>
2023-07-13 12:14:39 +0000

    Added profile form, views and updated template

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Order
 
+
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
@@ -34,5 +35,5 @@ class OrderForm(forms.ModelForm):
                 else:
                     placeholder = placeholders[field]
                 self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'stripe-style-inut'
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
